724c82729b428a2c2f46e19ca0fffa5ab7b834f8
Shiva Rekhan <52254431+shvrekhan@users.noreply.github.com>
2020-07-18 13:03:30 +0530

    Add files via upload

---
+++ Round 653(Div.3)/RequiredReminder.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+ 
+public class RequiredReminder{
+    static StringBuilder sb;
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        sb=new StringBuilder();
+        int t=sc.nextInt();
+        while(t-->0){
+            int x=sc.nextInt(),y=sc.nextInt(),n=sc.nextInt();
+                int z=n%x;
+                if(z>y){
+                    n-=(z-y);
+                    sb.append(n);
+                }
+                else if(z==y)sb.append(n);
+                else{
+                    n-=x;
+                    n+=(y-z);
+                    sb.append(n);
+                }
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+}
